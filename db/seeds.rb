51077edc9612435677e0551892f5ac454688bee8
Halima Lamliji <halima.lamliji@ymail.com>
2024-08-20 12:33:02 +0200

    creating seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,68 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+List.all.destroy
+
+Favorite.all.destroy
+
+Follow.all.destroy
+
+Tag.all.destroy
+
+
+lists = [
+  { name: "Christmas list", public: false, user_id: }
+  { name: "Christmas list", public: false, user_id: }
+  { name: "Christmas list", public: false, user_id: }
+  { name: "Christmas list", public: false, user_id: }
+  { name: "Christmas list", public: false, user_id: }
+  { name: "Christmas list", public: false, user_id: }
+  { name: "Christmas list", public: false, user_id: }
+]
+
+favorites = [
+  { name: "", description: "", price: , size: "", url: "", user_id:  ,list_id: }
+  { name: "", description: "", price: , size: "", url: "", user_id:  ,list_id: }
+  { name: "", description: "", price: , size: "", url: "", user_id:  ,list_id: }
+  { name: "", description: "", price: , size: "", url: "", user_id:  ,list_id: }
+  { name: "", description: "", price: , size: "", url: "", user_id:  ,list_id: }
+  { name: "", description: "", price: , size: "", url: "", user_id:  ,list_id: }
+  { name: "", description: "", price: , size: "", url: "", user_id:  ,list_id: }
+]
+
+follows = [
+  { follower_id: , followed_id: }
+  { follower_id: , followed_id: }
+  { follower_id: , followed_id: }
+  { follower_id: , followed_id: }
+  { follower_id: , followed_id: }
+  { follower_id: , followed_id: }
+  { follower_id: , followed_id: }
+]
+
+tags = [
+  { name: "", favorite_id: }
+  { name: "", favorite_id: }
+  { name: "", favorite_id: }
+  { name: "", favorite_id: }
+  { name: "", favorite_id: }
+  { name: "", favorite_id: }
+  { name: "", favorite_id: }
+]
+
+lists.each do |list|
+  List.new(list)
+end
+
+favorites.each do |favorite|
+
+end
+
+follows.each do |follow|
+
+end
+
+tags.each do |tag|
+
+end
