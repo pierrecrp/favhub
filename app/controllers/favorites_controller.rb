4bac216b76ad51ea6d1a740bc08444f5585ca750
Halima Lamliji <halima.lamliji@ymail.com>
2024-08-21 17:36:17 +0200

    update favorites index

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,10 +5,26 @@ class FavoritesController < ApplicationController
   def index
     @favorites = Favorite.where(user: current_user)
     @lists = List.where(user: current_user)
+
+    if params[:list_id].present?
+      @favorites = @favorites.where(list_id: params[:list_id])
+    else
+      @favorites = @favorites.where(list_id: nil)
+    end
+
   end
 
   def show
+  end
 
+  def create
+    @list = List.new(list_params)
+    @list.user = current_user
+    if @list.save
+      redirect_to favorites_path
+    else
+      render "favorites", status: :unprocessable_entity
+    end
   end
 
   def update
@@ -19,5 +35,9 @@ class FavoritesController < ApplicationController
 
   def set_favorite
     @favorite = Favorite.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :public)
   end
 end
